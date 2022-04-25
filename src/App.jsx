0897e3fb8ee286837ccc6f30ee2489435e1da0e8
Sameer Ajay Harankhedkar <95958744+sameerah2710@users.noreply.github.com>
2022-04-25 15:31:17 +0530

    c3 1st commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
+import { EmployeeList } from "./components/EmployeeList";
+import { EmployeeDetails } from "./components/EmployeeDetails";
+import { Admin } from "./components/Admin";
+import { ProtectedRoute } from "./components/PrivateRoute";
+import { Navbar } from "./components/Navbar";
+import { Logout } from "./components/Logout";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Navbar /> */}
+      {/* <>{/* Routes here * */}
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
